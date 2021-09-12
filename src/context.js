176b24a4d2bc27bce4bd85f88114f826f090fce1
rturner1989 <robturner1000@hotmail.co.uk>
2021-09-12 18:53:07 +0100

    File Structure Update
    - added components folder
    - added images folder

---
+++ src/context.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
