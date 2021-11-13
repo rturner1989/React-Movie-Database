4e1231f0049ce8bfd043ba1c68a0f085c559bc77
rturner1989 <robturner1000@hotmail.co.uk>
2021-11-13 18:15:13 +0000

    Update

--- src/components/FullScreenResult/tvfullscreenresult.js
+++ src/components/FullScreenResult/tvfullscreenresult.js
@@ -10,6 +10,7 @@ import {
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../../context";
 import FullScreenReview from "./fullscreenreview/fullscreenreview";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const TvFullScreenResult = () => {
     const { id } = useParams();
@@ -17,6 +18,9 @@ const TvFullScreenResult = () => {
     const [tvResultData, setTvResultData] = useState({});
     const [tvCreditData, setTvCreditData] = useState({});
     const [toggle, setToggle] = useState({ category: "cast" });
+    const [mobileTVToggle, setMobileTVToggle] = useState({
+        category: "overview",
+    });
     const {
         addToWatchList,
         removeFromWatchList,
@@ -27,6 +31,9 @@ const TvFullScreenResult = () => {
         modalContent,
         setModalContent,
     } = useGlobalContext();
+
+    const [windowDimensions] = useWindowDimensions();
+    const found = isTvShowInWatchlist(tvData.id);
 
     const getTvData = async () => {
         const response = await fetch(
@@ -58,7 +65,270 @@ const TvFullScreenResult = () => {
         getTvCredit();
     }, []);
 
-    const found = isTvShowInWatchlist(tvData.id);
+    const tvContentToggle = () => {
+        switch (mobileTVToggle.category) {
+            case "overview":
+                return (
+                    <div className="mobile-fullscreen-overview">
+                        <div className="mobile-fullscreen-list-info">
+                            <p className="mobile-fullscreen-release-date">
+                                {convertDate(tvData.release_date)}
+                            </p>
+                            <div className="ball"></div>
+                            <div className="mobile-fullscreen-genre">
+                                {tvData.genres !== undefined ? (
+                                    tvData.genres.map((type) => {
+                                        return <p key={type.id}>{type.name}</p>;
+                                    })
+                                ) : (
+                                    <p></p>
+                                )}
+                            </div>
+                            <div className="ball"></div>
+                            <p>{tvData.runtime} mins</p>
+                        </div>
+                        <div className="mobile-fullscreen-rating">
+                            <h4 className="mobile-average-score-title">
+                                Average Score:
+                            </h4>
+                            <p className="mobile-fullscreen-vote">
+                                <AiFillStar className="render-icon" />
+                                {tvData.vote_average}
+                            </p>
+                            <button
+                                className="add-remove-button"
+                                onClick={() => {
+                                    found
+                                        ? removeFromWatchList("tv", tvData.id)
+                                        : addToWatchList("tv", tvData);
+                                }}
+                            >
+                                {found ? (
+                                    <IoMdRemoveCircleOutline
+                                        className="remove-icon"
+                                        aria-hidden={true}
+                                        focusable={false}
+                                    />
+                                ) : (
+                                    <IoMdAddCircleOutline
+                                        className="add-icon"
+                                        aria-hidden={true}
+                                        focusable={false}
+                                    />
+                                )}
+                            </button>
+                        </div>
+                        <div className="mobile-fullscreen-production">
+                            {tvData.production_companies !== undefined ? (
+                                tvData.production_companies.map((company) => {
+                                    return (
+                                        <p
+                                            key={company.id}
+                                            className="mobile-company-name"
+                                        >
+                                            {company.name}
+                                        </p>
+                                    );
+                                })
+                            ) : (
+                                <p></p>
+                            )}
+                        </div>
+                        <h4 className="mobile-fullscreen-tagline">
+                            <i>{tvData.tagline}</i>
+                        </h4>
+                        <div className="mobile-fullscreen-overview-text">
+                            <h3>Overview</h3>
+                            <p className="mobile-fullscreen-overview-content">
+                                {tvData.overview}
+                            </p>
+                            <button
+                                className="mobile-expand-btn"
+                                onClick={() => {
+                                    setExpandBiography(!expandBiography);
+                                    setModalContent(tvData.overview);
+                                }}
+                            >
+                                <AiOutlineExpandAlt
+                                    className="expand-icon"
+                                    aria-hidden={true}
+                                    focusable={false}
+                                />
+                            </button>
+                        </div>
+                    </div>
+                );
+            case "cast":
+                return tvCreditData.cast !== undefined ? (
+                    <div className="mobile-fullscreen-cast">
+                        {tvCreditData.cast.map((cast) => {
+                            return (
+                                <div
+                                    key={cast.id}
+                                    className="mobile-cast-credit"
+                                >
+                                    {cast.profile_path === null ? (
+                                        <div className="mobile-cast-btn-icon-container">
+                                            <AiOutlineFileImage
+                                                className="mobile-cast-btn-icon"
+                                                aria-hidden={true}
+                                                focusable={false}
+                                            />
+                                        </div>
+                                    ) : (
+                                        <img
+                                            className="mobile-cast-img"
+                                            src={`https://image.tmdb.org/t/p/w500/${cast.profile_path}`}
+                                            alt=""
+                                        />
+                                    )}
+                                    <Link
+                                        to={`/result/people/${cast.id}`}
+                                        className="mobile-cast-role"
+                                    >
+                                        {cast.name}
+                                    </Link>
+                                    <p className="mobile-cast-name">
+                                        {cast.character}
+                                    </p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div></div>
+                );
+            case "review":
+                return tvResultData.results !== undefined ? (
+                    <div className="mobile-fullscreen-reviews">
+                        {tvResultData.results.map((result, index) => {
+                            return (
+                                <div key={index}>
+                                    <FullScreenReview
+                                        author={result.author}
+                                        written={convertDate(result.created_at)}
+                                        review={result.content}
+                                    />
+                                    <button
+                                        className="review-modal-expand-btn"
+                                        onClick={() => {
+                                            setExpandBiography(
+                                                !expandBiography
+                                            );
+                                            setModalContent(result.content);
+                                        }}
+                                    >
+                                        <AiOutlineExpandAlt
+                                            className="expand-icon"
+                                            aria-hidden={true}
+                                            focusable={false}
+                                        />
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div></div>
+                );
+            default:
+                break;
+        }
+    };
+
+    if (windowDimensions.width <= 950) {
+        return (
+            <div className="mobile-fullscreen">
+                <div className="mobile-fullscreen-card">
+                    <img
+                        className="mobile-fullscreen-img"
+                        src={`https://image.tmdb.org/t/p/w500/${tvData.poster_path}`}
+                        alt=""
+                    />
+                    <div className="mobile-fullscreen-section-container">
+                        <h1 className="mobile-fullscreen-title">
+                            {tvData.name}
+                        </h1>
+                        <nav className="mobile-fullscreen-header-container">
+                            <ul className="mobile-fullscreen-header">
+                                <li
+                                    className={`mobile-toggle-btn mobile-overview-btn ${
+                                        mobileTVToggle.category === "overview"
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={() =>
+                                        setMobileTVToggle({
+                                            category: "overview",
+                                        })
+                                    }
+                                    disabled={
+                                        mobileTVToggle.category === "overview"
+                                    }
+                                >
+                                    Overview
+                                </li>
+                                <li
+                                    className={`mobile-toggle-btn mobile-cast-btn ${
+                                        mobileTVToggle.category === "cast"
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={() =>
+                                        setMobileTVToggle({
+                                            category: "cast",
+                                        })
+                                    }
+                                    disabled={
+                                        mobileTVToggle.category === "cast"
+                                    }
+                                >
+                                    Cast
+                                </li>
+                                <li
+                                    className={`mobile-toggle-btn mobile-review-btn ${
+                                        mobileTVToggle.category === "review"
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={() =>
+                                        setMobileTVToggle({
+                                            category: "review",
+                                        })
+                                    }
+                                    disabled={
+                                        mobileTVToggle.category === "review"
+                                    }
+                                >
+                                    Review
+                                </li>
+                            </ul>
+                        </nav>
+                        {tvContentToggle()}
+                    </div>
+                    <div
+                        className={
+                            expandBiography
+                                ? "modal-biography-active"
+                                : "modal-biography-hidden"
+                        }
+                    >
+                        <button
+                            className="model-exit-btn"
+                            onClick={() => setExpandBiography(false)}
+                        >
+                            <AiOutlineClose
+                                className="model-btn-icon"
+                                aria-hidden={true}
+                                focusable={false}
+                            />
+                        </button>
+                        <p className="modal-biography">{modalContent}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="fullscreen">
